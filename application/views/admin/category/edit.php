c8fcb4b7583b5bf2095eb8b6782c8f1e83bbdf21
quangteo168 <quangcute168@gmail.com>
2016-11-13 08:25:05 +0700

    update

--- application/views/admin/category/edit.php
+++ application/views/admin/category/edit.php
@@ -6,15 +6,15 @@
                 <tr>
                     <td width="30%"></td>
                     <td width="10%">tên danh mục</td>
-                    <td width="30%"><input tabindex="1" required type="text" name="txt_name" size="50"></td>
+                    <td width="30%"><input tabindex="1" required type="text" name="txt_title" size="50" value="<?php echo $item['category_name']; ?>"></td>
                     <td width="30%"></td>
                 </tr>
                 <tr>
                     <td></td>
                     <td>kích hoạt</td>
                     <td>
-                        <input tabindex="10" checked="checked" type="radio" name="active"> Có
-                        <input tabindex="11" type="radio" name="active"> Không
+                        <input tabindex="2" type="radio" name="active" value="1" <?php echo($item['active'] == 1)?'checked':''; ?>> Có
+                        <input tabindex="3" type="radio" name="active" value="0" <?php echo($item['active'] == 0)?'checked':''; ?>> Không
                     </td>
                     <td></td>
                 </tr>
@@ -22,9 +22,17 @@
                     <td></td>
                     <td>Thuộc ngành</td>
                     <td>
-                        <select name="sl">
-                            <option value="1">Ngành 1</option>
-                            <option value="2">Ngành 2</option>
+                        <select name="cat" id="cat" tabindex="4">
+                            <option value="0">Lựa chọn ngành</option>
+                            <?php
+                                foreach($list as $key=>$val):
+                                    if($val['parent'] == 0):
+                            ?>
+                                        <option <?php echo ($item['parent'] == $val['category_id'])?'selected':''; ?> value="<?php echo $val['category_id']; ?>"><?php echo $val['category_name']; ?></option>
+                            <?php
+                                    endif;
+                                endforeach;
+                            ?>
                         </select>
                     </td>
                     <td></td>
@@ -32,16 +40,19 @@
                 <tr>
                     <td></td>
                     <td>mô tả</td>
-                    <td><textarea tabindex="13" name="description" id="description" cols="38" rows="5"></textarea></td>
+                    <td><textarea tabindex="5" name="description" id="description" cols="38" rows="5"><?php echo $item['category_description']; ?></textarea></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td colspan="4">
-                        <input tabindex="14" type="submit" name="sumit" value="Cập nhật">
-                        <input tabindex="15" type="reset" value="Làm Lại">
+                        <input tabindex="6" type="submit" name="submit" value="Cập nhật">
+                        <input tabindex="7" type="reset" value="Làm Lại">
                     </td>
                 </tr>
             </table>
         </form>
     </div>
 </div>
+<?php if(isset($error)){ ?>
+    <script>alert('<?php echo $error; ?>')</script>
+<?php } ?>
