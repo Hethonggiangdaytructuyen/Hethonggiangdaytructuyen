f204e8aaee98bd3d3434b53aecee1d772d91ae11
quangteo168 <quangcute168@gmail.com>
2016-11-13 08:22:13 +0700

    update

--- application/views/admin/article/article.php
+++ application/views/admin/article/article.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-    .collapsed-box{ height: 360px }
+    .collapsed-box{ height: 1080px }
 </style>
 <h2>Bài đăng</h2>
 <div id="main">
@@ -11,239 +11,116 @@
         <div class="minimize">
             <button class="close">X</button>
         </div>
-        <form action="" method="post" name="form">
+        <form action="" method="post" name="form" enctype="multipart/form-data">
             <table class="table_form">
                 <tr>
                     <td width="30%"></td>
                     <td width="10%">tiêu đề</td>
-                    <td width="30%"><input tabindex="1" required type="text" name="txt_title" size="50"></td>
+                    <td width="30%"><input tabindex="1" required type="text" name="txt_title" size="50" value="<?php echo set_value('txt_title'); ?>"></td>
                     <td width="30%"></td>
                 </tr>
                 <tr>
                     <td></td>
                     <td>avatar</td>
-                    <td><input tabindex="2" required type="file" name="image"></td>
+                    <td><input tabindex="2" required type="file" name="article_image"><input type="hidden" name="article_image" value="<?php echo set_value('article_image'); ?>"></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td></td>
                     <td>tác giả</td>
-                    <td><input tabindex="3" required type="text" name="txt_author" size="50"></td>
+                    <td>
+                        <input disabled="true" type="text" name="txt_author" tabindex="3" required value="<?php echo $this->session->userdata('user_fullname'); ?>">
+                    </td>
                     <td></td>
                 </tr>
                 <tr>
                     <td></td>
                     <td>môn học</td>
-                    <td><input tabindex="4" required type="text" name="txt_subject" size="50"></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>kích hoạt</td>
                     <td>
-                        <input tabindex="5" checked="checked" type="radio" name="active"> Có
-                        <input tabindex="6" type="radio" name="active"> Không
+                        <select tabindex="4" name="subject_cat" id="subject_cat">
+                            <?php
+                                foreach($subject_list as $subject_key=>$subject_value):
+                            ?>
+                                    <option <?php echo (set_value('subject_cat') == $subject_value['subject_id'])?'selected':''; ?> value="<?php echo $subject_value['subject_id']; ?>"><?php echo $subject_value['subject_name']; ?></option>
+                            <?php
+                                endforeach;
+                            ?>
+                        </select>
                     </td>
                     <td></td>
                 </tr>
+                <?php if($this->session->userdata('user_role_id') == 1){ ?>
+                    <tr>
+                        <td></td>
+                        <td>kích hoạt</td>
+                        <td>
+                            <input tabindex="5" type="radio" name="active" value="1" <?php echo (set_value('active') == 1)?'checked':''; ?>>Có
+                            <input tabindex="6" type="radio" name="active" value="0" <?php echo (set_value('active') == 0)?'checked':''; ?>>Không
+                        </td>
+                        <td></td>
+                    </tr>
+                <?php } ?>
                 <tr>
                     <td></td>
-                    <td>có thể xóa</td>
-                    <td>
-                        <input tabindex="7" checked="checked" type="radio" name="active"> Có
-                        <input tabindex="8" type="radio" name="active"> Không
-                    </td>
+                    <td></td>
+                    <td class="bold uppercase">nội dung bài viết</td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td>nội dung</td>
-                    <td><textarea tabindex="9" name="content" id="content" cols="38" rows="5"></textarea></td>
-                    <td></td>
+                    <td colspan="4"><textarea tabindex="7" name="content" id="content"><?php echo set_value('content'); ?></textarea></td>
                 </tr>
                 <tr>
                     <td colspan="4">
-                        <input tabindex="10" type="submit" name="sumit" value="Thêm Mới">
-                        <input tabindex="11" type="reset" value="Làm Lại">
+                        <input tabindex="8" type="submit" name="submit" value="Thêm Mới">
+                        <input tabindex="9" type="reset" value="Làm Lại">
                     </td>
                 </tr>
             </table>
         </form>
     </div>
 
+    <script type="text/javascript">
+        CKEDITOR.replace('content');
+    </script>
+
     <table class="list" border="1" cellpadding="0" cellspacing="0">
         <thead>
             <tr class="title">
                 <td width="2%">id</td>
-                <td width="19%">email</td>
-                <td width="10%">tên đăng nhập</td>
-                <td width="15%">họ tên</td>
-                <td width="10%">điện thoại</td>
-                <td width="20%">địa chỉ</td>
-                <td width="5%">giới tính</td>
+                <td width="19%">tiêu đề</td>
+                <td width="10%">tác giả</td>
+                <td width="15%">môn học</td>
+                <td width="10%">ngày tạo</td>
+                <td width="20%">nội dung</td>
                 <td width="5%">kích hoạt</td>
-                <td width="8%">quyền hạn</td>
+                <td width="8%">ảnh giới thiệu</td>
                 <td width="3%">sửa</td>
                 <td width="3%">xóa</td>
             </tr>
         </thead>
 
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Admin</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>2</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Admin</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>3</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Author</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>4</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Advertiser</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>5</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Customer</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>6</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Customer</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>7</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Customer</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>8</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Advertiser</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>9</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Author</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
-
-            <tr>
-                <td>10</td>
-                <td>phanvanngockd@gmail.com</td>
-                <td>trungtc</td>
-                <td>Phan Văn Ngọc</td>
-                <td>0168 3535 845</td>
-                <td>Nghĩa Đô, Cầu Giấy, Hà Nội</td>
-                <td>Nam</td>
-                <td>Có</td>
-                <td>Author</td>
-                <td><i class="fa fa-pencil fa-lg"></i></td>
-                <td><i class="fa fa-trash fa-lg"></i></td>
-            </tr>
+            <?php
+                foreach($list as $key=>$val):
+            ?>
+                    <tr>
+                        <td><?php echo $val['article_id']; ?></td>
+                        <td><?php echo $val['article_name']; ?></td>
+                        <td><?php echo $val['user_fullname']; ?></td>
+                        <td><?php echo $val['subject_name']; ?></td>
+                        <td><?php echo $val['created_date']; ?></td>
+                        <td><?php echo substr($val['article_content'],0,100); ?></td>
+                        <td><?php echo ($val['active'] == 1)?'Kích hoạt':'Không kích hoạt'; ?></td>
+                        <td><img width="60" src="template/images/admin/<?php echo $val['article_image']; ?>" /></td>
+                        <td><a <?php if($val['active'] == 0){ ?> href="./admin/edit_article/<?php echo $val['article_id']; ?>" <?php }else{ ?> class="ban" onclick="return confirm('Bạn không thể sửa bài viết đã được kích hoạt!')" <?php } ?>><i class="fa fa-pencil fa-lg"></i></a></td>
+                        <td><i class="fa fa-trash fa-lg"></i></td>
+                    </tr>
+            <?php
+                endforeach;
+            ?>
         </tbody>
     </table>
-
-    <div class="pagination">
-        <table>
-            <tr>
-                <td width="80%"></td>
-                <td><a href="#">First</a></td>
-                <td><a href="#"><<</a></td>
-                <td class="number active">1</td>
-                <td class="number"><a href="#">2</a></td>
-                <td class="number"><a href="#">3</a></td>
-                <td class="number"><a href="#">4</a></td>
-                <td><a href="#">>></a></td>
-                <td><a href="#">Last</a></td>
-            </tr>
-        </table>
-    </div>
 </div>
+<?php if(isset($error)){ ?>
+    <script>alert('<?php echo $error; ?>')</script>
+<?php } ?>
